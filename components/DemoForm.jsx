4e3134b3819f30c747c85fab6d11930a6f079012
Kalcho8233 <nikolavladimirov017@gmail.com>
2025-07-11 18:25:26 +0300

    Update DemoForm.jsx

--- components/DemoForm.jsx
+++ components/DemoForm.jsx
@@ -13,7 +13,7 @@ const handleSubmit = async (e) => {
   };
 
   try {
-    const res = await fetch('https://omninex.app.n8n.cloud/webhook-test/3b65c89f-d4c7-499f-b9ea-804af9f9dfc5', {
+    const res = await fetch('https://omninex.app.n8n.cloud/webhook/3b65c89f-d4c7-499f-b9ea-804af9f9dfc5', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(submittedData),
