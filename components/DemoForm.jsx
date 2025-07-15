8bb96636a6cab1bbc1bae38a5ec07d29835e5f93
Kalcho8233 <nikolavladimirov017@gmail.com>
2025-07-15 12:12:16 +0300

    Update DemoForm.jsx

--- components/DemoForm.jsx
+++ components/DemoForm.jsx
@@ -25,6 +25,7 @@ export default function DemoForm() {
 
     try {
       await fetch('https://kolio.app.n8n.cloud/webhook-test/omninexai', {
+
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(submittedData),
