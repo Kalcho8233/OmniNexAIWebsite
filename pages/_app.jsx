59ffe967d602fd72921a52cce6c471b9ea3ded80
Kalcho8233 <nikolavladimirov017@gmail.com>
2025-08-15 15:28:36 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,25 @@
 // pages/_app.jsx
-import '../styles/globals.css';
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Favicons */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+        {/* Apple / PWA */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#0b0f19" />
+
+        {/* По избор: SEO/UX */}
+        <meta name="application-name" content="OmniNex" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
